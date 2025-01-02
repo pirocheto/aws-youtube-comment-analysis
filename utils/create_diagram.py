f9b239540866426312bd7dd78b68dea8227af7fb
pirocheto <pirocheto@gmail.com>
2025-01-02 13:11:23 +0100

    fix table name

---
+++ utils/create_diagram.py
@@ -0,0 +1,21 @@
+from diagrams import Diagram
+from diagrams.aws.analytics import Athena, GlueDataCatalog
+from diagrams.aws.compute import Lambda
+from diagrams.aws.ml import Comprehend
+from diagrams.aws.security import SecretsManager
+from diagrams.aws.storage import S3
+
+with Diagram(
+    "Youtube Comment Sentiment Analysis", show=False, filename="img/architecture"
+):
+    # Resources
+    lambda_function = Lambda("Lambda Function")
+    s3_bucket = S3("S3 Bucket")
+    secrets_manager = SecretsManager("AWS Secrets Manager")
+    comprehend = Comprehend("AWS Comprehend")
+    glue_data_catalog = GlueDataCatalog("Glue Data Catalog")
+    athena = Athena("Athena")
+
+    # Relationships
+    lambda_function >> [s3_bucket, comprehend, secrets_manager]
+    athena >> glue_data_catalog >> s3_bucket
