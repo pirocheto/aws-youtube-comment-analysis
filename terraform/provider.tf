b2a958b2fb92d5a3c1515dc1a240e94a41f35864
pirocheto <pirocheto@gmail.com>
2025-01-06 17:16:09 +0100

    add s3 backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,12 @@ terraform {
   }
 
   required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket = "youtube-comments-terraform-state"
+    key    = "${env}-terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
