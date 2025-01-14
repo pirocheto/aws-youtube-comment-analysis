3fed8b0ef052b6940762e9260d369fe37b621438
pirocheto <pirocheto@gmail.com>
2025-01-15 00:21:41 +0100

    fix provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,27 +14,3 @@ terraform {
     region = "us-east-1"
   }
 }
-
-provider "aws" {
-  region = var.aws_region
-}
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  required_version = ">= 1.3.0"
-
-  backend "s3" {
-    bucket = "terraform-states"
-    key    = "youtube-comment-analysis/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
